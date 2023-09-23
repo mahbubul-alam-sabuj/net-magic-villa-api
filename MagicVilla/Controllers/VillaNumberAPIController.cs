bf1af6ff120f2af547397713de9eda7b6625c9ed
Mahbubul Alam Sabuj <mahbubul.alam@selise.ch>
2023-09-23 23:48:04 +0600

    update MappingConfig

--- MagicVilla/Controllers/VillaNumberAPIController.cs
+++ MagicVilla/Controllers/VillaNumberAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using AutoMapper;
 using MagicVilla.Models;
+using MagicVilla.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla.Controllers;
@@ -60,11 +61,72 @@ public class VillaNumberAPIController : ControllerBase
         _response.IsSuccess = false;
         return NotFound(_response);
       }
+      _response.StatusCode = HttpStatusCode.OK;
       _response.Result = villaNumber;
       return Ok(_response);
     }
     catch (Exception ex)
     {
+      _response.IsSuccess = false;
+      _response.ErrorMessages = new List<string> { ex.ToString() };
+    }
+    return _response;
+  }
+
+  [HttpPost()]
+  [ProducesResponseType(StatusCodes.Status201Created)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<ActionResult<APIResponse>> CreateVillaNumber([FromBody] VillaNumberCreateDTO villaNumberCreateDTO)
+  {
+    _logger.LogInformation("Creating villa number");
+    try
+    {
+      if (villaNumberCreateDTO == null)
+      {
+        _response.StatusCode = HttpStatusCode.BadRequest;
+        _response.IsSuccess = false;
+        return BadRequest(_response);
+      }
+      VillaNumber newVillaNumber = _mapper.Map<VillaNumber>(villaNumberCreateDTO);
+      _response.StatusCode = HttpStatusCode.Created;
+      _response.Result = _mapper.Map<VillaNumberDTO>(newVillaNumber); 
+      await _dbVillaNumber.CreateAsync(newVillaNumber);
+      await _dbVillaNumber.SaveAsync();
+      return CreatedAtRoute(nameof(GetVillaNumbers), new { VillaNumber = newVillaNumber.VillaNo }, _response);
+    }
+    catch (Exception ex)
+    {
+      _response.IsSuccess = false;
+      _response.StatusCode = HttpStatusCode.InternalServerError;
+      _response.ErrorMessages = new List<string> { ex.ToString() };
+    }
+    return _response;
+  }
+
+  [HttpDelete("{id}")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+  public async Task<ActionResult<APIResponse>> DeleteVillaNumber(int id)
+  {
+    _logger.LogInformation($"Deleting villa number with id = {id}");
+    try
+    {
+      VillaNumber? villaNumber = await _dbVillaNumber.GetAsync((villaNumber) => villaNumber.VillaNo == id);
+      if (villaNumber == null)
+      {
+        _response.StatusCode = HttpStatusCode.NotFound;
+        _response.IsSuccess = false;
+        return NotFound(_response);
+      }
+      _response.StatusCode = HttpStatusCode.NoContent;
+      _response.IsSuccess = true;
+      return Ok(_response);
+    }
+    catch (Exception ex)
+    {
+      _response.StatusCode = HttpStatusCode.InternalServerError;
       _response.IsSuccess = false;
       _response.ErrorMessages = new List<string> { ex.ToString() };
     }
--- MagicVilla/MappingConfig.cs
+++ MagicVilla/MappingConfig.cs
@@ -8,10 +8,12 @@ public class MappingConfig : Profile
 {
   public MappingConfig()
   {
-    CreateMap<Villa, VillaDTO>();
-    CreateMap<VillaDTO, Villa>();
-
+    CreateMap<Villa, VillaDTO>().ReverseMap();
     CreateMap<Villa, VillaCreateDTO>().ReverseMap();
     CreateMap<Villa, VillaUpdateDTO>().ReverseMap();
+
+    CreateMap<VillaNumber, VillaNumberDTO>().ReverseMap();
+    CreateMap<VillaNumber, VillaNumberCreateDTO>().ReverseMap();
+    CreateMap<VillaNumber, VillaNumberUpdateDTO>().ReverseMap();
   }
 }
